a1f65118ae145d4f52f3b96cece5bfcb04b27518
OG <103863031+RyzKum@users.noreply.github.com>
2023-01-25 10:32:39 +0100

    Score uptdate à check

--- src/Score.java
+++ src/Score.java
@@ -2,50 +2,51 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedWriter;
 
-public class Score {
+
+public class Score{
+    // propriété privée pour stocker le score
     private int score;
-    private int joueur;
-    
-    public Score(int score, int joueur) {
-        this.score = score;
-        this.joueur = joueur;
-    }
-    
-    public int getScore() {
-        return score;
-    }
-    
-    public void setScore(int score) {
-        this.score = score;
-    }
-    
-    public int getJoueur() {
+    // propriété privée pour stocker l'objet Joueur
+    private Joueur joueur;
+
+    // méthode pour récupérer l'objet Joueur
+    public Joueur getJoueur () {
         return joueur;
     }
-    
-    public void setJoueur(int joueur) {
+
+    // méthode pour définir l'objet Joueur
+    public void setJoueur (Joueur joueur) {
         this.joueur = joueur;
     }
-    
-    public void afficherScore() {
-        System.out.println("Le score du joueur " + this.joueur + " est de " + this.score);
+
+    // méthode pour récupérer la valeur du score
+    public int getScore () {
+        return score;
     }
-    
-    // Le score du joueur dans un fichier csv
+
+    // méthode pour définir la valeur du score
+    public void setScore (int score) {
+        this.score = score;
+    }
+
     public void afficherScoreCSV() {
         try {
-            FileWriter fw = new FileWriter("score.csv", true);
+            //Création ou ajout à un fichier CSV pour enregistrer le score du joueur
+            FileWriter fw = new FileWriter("fichier.csv", true);
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write(String.valueOf(this.score));
+            //Ajout d'une nouvelle ligne pour chaque entrée de score
             bw.newLine();
             bw.close();
         } catch (IOException e) {
+            //Affichage d'un message d'erreur et fermeture du programme en cas d'exception lors de l'écriture dans le fichier
             e.printStackTrace();
             System.out.println("Erreur lors de l'écriture du fichier");
             System.exit(0);
         }
+        //Affichage du score du joueur à l'écran et fermeture du programme
         System.out.println("Le score du joueur " + this.joueur + " est de " + this.score);
         System.exit(0);
-
     }
+    
 }
